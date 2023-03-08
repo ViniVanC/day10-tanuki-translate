9d3159b20f6c197bf1fefa35491e3a2160083723
ViniVanC <vincentvan0302@gmail.com>
2023-03-08 18:04:47 +0200

    added Container to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Container } from "./components/Container";
+
 function App() {
   return (
-    <div className="flex h-[100vh] items-center justify-center text-[400px] font-black">
-      hi{" "}
+    <div className="flex h-[100vh] items-center justify-center bg-gradient-teal px-[15px]">
+      <Container />
     </div>
   );
 }
